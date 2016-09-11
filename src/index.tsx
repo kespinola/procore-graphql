7305c4f3d6b30a176c3047e0283fe2eead10e3c3
Faris Mustafa <farismmk@gmail.com>
2016-09-11 16:30:05 -0700

    more cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,7 @@ import './index.scss'
 import * as React from 'react'
 import { render } from 'react-dom'
 
-import { BoxGeometry, Mesh } from './three'
-
 render(
-  <div>
-    <Mesh>
-      <BoxGeometry width={1} height={1} depth={1} />
-    </Mesh>
-  </div>,
+  <div />,
   document.getElementById('root')
 )
