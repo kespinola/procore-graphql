f8dccb3b1c986ad7c93985b28c87908b94eb7216
Faris Mustafa <farismmk@gmail.com>
2016-09-11 16:24:11 -0700

    clean up a bit

--- src/three/components/Mesh.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export interface IMeshProps extends React.HTMLProps<HTMLDivElement> {}
-
-export default ({ children }: IMeshProps) => (
-  <span>{children}</span>
-)
